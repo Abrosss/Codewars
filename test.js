9b8cff4203800471065a2c4e0345cc1f7e8eabc0
Abrosss <nataroro5@gmail.com>
2022-11-03 15:07:10 +0600

    6

--- test.js
+++ test.js
@@ -18,3 +18,30 @@ function maskify(cc) {
 function solution(str, ending){
    return str.endsWith(ending)
   }
+
+function order(words){
+    if(words == "") return ""
+    else {
+        let wordsArray = words.split(' ')
+        let numbers = []
+        let final = []
+        for(let i = 0; i<wordsArray.length; i++) {
+    
+            let number = wordsArray[i].match(/\d/)
+            numbers.push({number: Number(number[0]),text: number.input})
+            numbers.sort((a,b)=>a.number-b.number)
+          
+        }
+       numbers.forEach(num=> final.push(num.text))
+       return final.join(' ')
+    }
+    
+  }
+  function order2(words){
+    return words.split(' ').sort((a,b)=>a.match(/\d/)-b.match(/\d/)).join(" ")
+    }
+    
+  
+
+  console.log(order2("4of Fo1r pe6ople g3ood th5e the2"))
+  console.log(order2(""))

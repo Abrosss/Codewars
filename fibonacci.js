18c53ac7f0b7819e8208f33d314479149591c402
Natalia <84074575+Abrosss@users.noreply.github.com>
2023-02-08 23:26:09 +0600

    Create fibonacci.js

---
+++ fibonacci.js
@@ -0,0 +1,18 @@
+function fib() {
+    cache ={}
+    return function inner (n) {
+        if (n in cache) {
+            return cache[n];
+          }
+          if (n <= 1) {
+            return n;
+          } else {
+            const result = inner(n - 1) + inner(n - 2);
+            console.log(result)
+            cache[n] = result;
+        
+            return result;
+          }
+    }
+  
+}

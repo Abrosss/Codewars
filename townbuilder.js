fe4c1bf4808171dc2cee4ed2d1b34cd856807d3a
Abrosss <nataroro5@gmail.com>
2023-04-06 22:55:04 +0600

    added challenge

---
+++ townbuilder.js
@@ -0,0 +1,9 @@
+function towerBuilder(n) {
+    return Array.from({length: n}, function(v, k) {
+    
+      const spaces = ' '.repeat(n - k - 1);
+    
+      return spaces + '*'.repeat(k + k + 1) + spaces;
+    });
+  }
+  console.log(towerBuilder(6))
